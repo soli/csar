9296471b0917e492c7bacebcbb60d5d9475bf637
Sylvain Soliman <Sylvain.Soliman@gmail.com>
2014-07-20 13:54:02 +0200

    working version with RefCells

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 use std::fmt;
 // #![feature(managed_boxes)]
-// use std::cell::RefCell;
+use std::cell::RefCell;
 
 #[allow(dead_code)]
 fn main() {
@@ -15,10 +15,14 @@ fn main() {
 
 /// Representation of finite domains as a list of intervals, maintaining
 /// min and max for easy/quick access
-struct Domain {
+struct Dom {
     min: int,
     max: int,
     intervals: Vec<(int, int)>
+}
+
+struct Domain {
+    dom: RefCell<Dom>
 }
 
 pub trait Propagator : ToStr {
@@ -27,6 +31,7 @@ pub trait Propagator : ToStr {
     fn unregister(&self);
 }
 
+#[allow(dead_code)]
 pub struct FDVar {
     name: String,
     dom: Domain,
@@ -42,53 +47,71 @@ pub enum Event {
     Ins
 }
 
+#[allow(dead_code)]
 impl Domain {
     /// Domain created with initial bounds
     fn new(min: int, max: int) -> Domain {
         Domain {
-            min: min,
-            max: max,
-            intervals: vec![(min, max)]
+            dom: RefCell::new(Dom {
+                     min: min,
+                     max: max,
+                     intervals: vec![(min, max)]
+                 })
         }
     }
 
-    fn set_min(&mut self, min: int) {
-        if min < self.min { return; }
-        if min > self.max { return; } // TODO failure via conditions
+    fn set_min(&self, min: int) {
+        let mut dom = self.dom.borrow_mut();
+        if min < dom.min { return; }
+        if min > dom.max { return; } // TODO failure via conditions
         loop {
-            match self.intervals.get(0) {
+            match dom.intervals.get(0) {
                 // note that the breaks are for the loop, not the matching
-                &(x, _) if min < x => { self.min = x; break; },
-                &(_, y) if min > y => { self.intervals.shift(); },
-                &(_, y) => { self.min = min; *self.intervals.get_mut(0) = (min, y); break }
+                &(x, _) if min < x => { dom.min = x; break; },
+                &(_, y) if min > y => { dom.intervals.shift(); },
+                &(_, y) => {
+                    dom.min = min;
+                    *dom.intervals.get_mut(0) = (min, y);
+                    break;
+                }
             }
         }
     }
 
-    fn set_max(&mut self, max: int) {
-        if max > self.max { return; }
-        if max < self.min { return; } // TODO failure via conditions
+    fn get_min(&self) -> int {
+        self.dom.borrow().min
+    }
+
+    fn set_max(&self, max: int) {
+        let mut dom = self.dom.borrow_mut();
+        if max > dom.max { return; }
+        if max < dom.min { return; } // TODO failure via conditions
         loop {
-            match self.intervals.get(self.intervals.len() - 1) {
-                &(_, y) if max > y => { self.max = y; break; },
-                &(x, _) if max < x => { self.intervals.pop(); },
+            match dom.intervals.last().unwrap() {
+                &(_, y) if max > y => { dom.max = y; break; },
+                &(x, _) if max < x => { dom.intervals.pop(); },
                 &(x, _) => {
-                    self.max = max;
-                    *self.intervals.mut_last().unwrap() = (x, max);
+                    dom.max = max;
+                    *dom.intervals.mut_last().unwrap() = (x, max);
                     break
                 }
             }
         }
     }
 
-    fn remove(&mut self, val: int) {
-        if val > self.max || val < self.min { return; }
+    fn get_max(&self) -> int {
+        self.dom.borrow().max
+    }
+
+    fn remove(&self, val: int) {
+        let mut dom = self.dom.borrow_mut();
+        if val > dom.max || val < dom.min { return; }
         let mut down = 0;
-        let mut up = self.intervals.len();
+        let mut up = dom.intervals.len();
         let mut test;
         loop {
             test = down + (up - down) / 2;
-            match self.intervals.get(test) {
+            match dom.intervals.get(test) {
                 &(x, _) if val < x => {
                     if test > down {
                         up = test;
@@ -104,31 +127,31 @@ impl Domain {
                     }
                 },
                 &(x, y) if val == x && val == y => {
-                    self.intervals.remove(test);
+                    dom.intervals.remove(test);
                     break;
                 },
                 &(x, y) if val == x => {
-                    *self.intervals.get_mut(test) = (x + 1, y);
+                    *dom.intervals.get_mut(test) = (x + 1, y);
                     break;
                 },
                 &(x, y) if val == y => {
-                    *self.intervals.get_mut(test) = (x, y - 1);
+                    *dom.intervals.get_mut(test) = (x, y - 1);
                     break;
                 },
                 &(x, y) => {
-                    *self.intervals.get_mut(test) = (x, val - 1);
-                    self.intervals.insert(test + 1, (val + 1, y));
+                    *dom.intervals.get_mut(test) = (x, val - 1);
+                    dom.intervals.insert(test + 1, (val + 1, y));
                     break;
                 }
             }
         }
         if test == 0 {
-            match self.intervals.get(test) {
-                &(x, _) => self.min = x
+            match dom.intervals.get(test) {
+                &(x, _) => dom.min = x
             }
-        } else if test == self.intervals.len() - 1 {
-            match self.intervals.get(test) {
-                &(_, y) => self.max = y
+        } else if test == dom.intervals.len() - 1 {
+            match dom.intervals.get(test) {
+                &(_, y) => dom.max = y
             }
         }
     }
@@ -136,19 +159,22 @@ impl Domain {
 
 impl fmt::Show for Domain {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut s = format!("({}, {}) [", self.min, self.max);
-        for &(min, max) in self.intervals.iter() {
+        let dom = self.dom.borrow();
+        let mut s = format!("({}, {}) [", dom.min, dom.max);
+        for &(min, max) in dom.intervals.iter() {
             s = s + min.to_str() + ".." + max.to_str() + ", ";
         }
         return write!(f, "{}]", s);
     }
 }
 
+#[allow(dead_code)]
+#[allow(unused_variable)]
 impl FDVar {
-    pub fn new(min: int, max: int, name: String) -> FDVar {
+    pub fn new(min: int, max: int, name: &str) -> FDVar {
         assert!(min <= max);
         FDVar {
-            name: name,
+            name: name.to_string(),
             dom: Domain::new(min, max),
             waitingOnMin: vec![],
             waitingOnMax: vec![],
@@ -157,11 +183,11 @@ impl FDVar {
     }
 
     pub fn min(&self) -> int {
-        self.dom.min
+        self.dom.get_min()
     }
 
     pub fn max(&self) -> int {
-        self.dom.max
+        self.dom.get_max()
     }
 
     fn set_min(&mut self, v: int) -> Vec<Event> {
@@ -177,7 +203,7 @@ impl FDVar {
         }
     }
 
-    fn set_max(&mut self, v: int) -> Vec<Event> {
+    fn set_max(&self, v: int) -> Vec<Event> {
         if v < self.max() {
             self.dom.set_max(v);
             if self.is_instanciated() {
@@ -213,13 +239,13 @@ impl fmt::Show for FDVar {
     }
 }
 
-pub struct LtXYx {
-    x: FDVar,
-    y: FDVar
+pub struct LtXYx<'r> {
+    x: &'r FDVar,
+    y: &'r FDVar
 }
 
-impl LtXYx {
-    pub fn new(x: FDVar, y: FDVar) -> LtXYx {
+impl<'r> LtXYx<'r> {
+    pub fn new(x: &'r FDVar, y: &'r FDVar) -> LtXYx<'r> {
         let mut this = LtXYx { x: x, y: y };
         this.register();
         this.propagate();
@@ -227,37 +253,31 @@ impl LtXYx {
     }
 }
 
-impl Propagator for LtXYx {
+impl<'r> Propagator for LtXYx<'r> {
     fn register(&self) {
-        let mut y = self.y.borrow_mut();
-        y.get().add_waiting_max(self);
+        self.y.add_waiting_max(self);
     }
 
     fn unregister(&self) {
-        let mut y = self.y.borrow_mut();
-        y.get().del_waiting_max(self);
+        self.y.del_waiting_max(self);
     }
 
     fn propagate(&mut self) -> Vec<Event> {
-        let mut xx = self.x.borrow_mut();
-        let x = xx.get();
-        let mut yy = self.y.borrow_mut();
-        let y = yy.get();
-        if x.max() < y.min() {
+        if self.x.max() < self.y.min() {
             // entailed
             self.unregister();
-            []
-        } else if x.max() > y.max() - 1 {
+            vec![]
+        } else if self.x.max() > self.y.max() - 1 {
             //if y.is_instanciated() {
             //   self.unregister();
             //}
-            x.set_max(y.max() - 1)
+            self.x.set_max(self.y.max() - 1)
         } else {
-            []
+            vec![]
         }
     }
 }
-impl fmt::Show for LtXYx {
+impl<'r> fmt::Show for LtXYx<'r> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} < {}", self.x.to_str(), self.y.to_str())
     }
--- src/tests.rs
+++ src/tests.rs
@@ -1,162 +1,169 @@
 use super::FDVar;
 use super::Domain;
+use super::Dom;
 use super::LtXYx;
-// use std::cell::RefCell;
+use std::cell::RefCell;
 
 #[test]
 fn creates_new_var() {
-   let x = FDVar::new(-2, 255, "x");
-   assert!(x.min() == -2);
-   assert!(x.max() == 255);
+    let x = FDVar::new(-2, 255, "x");
+    assert!(x.min() == -2);
+    assert!(x.max() == 255);
 }
 
 fn min_is_min(d: &Domain) -> bool {
-   match d.intervals[0] {
-      (x, _) => x == d.min
-   }
+    match d.dom.borrow().intervals.get(0) {
+        &(x, _) => x == d.get_min()
+    }
 }
 
 fn max_is_max(d: &Domain) -> bool {
-   match d.intervals[d.intervals.len() - 1] {
-      (_, y) => y == d.max
-   }
+    match d.dom.borrow().intervals.last() {
+        Some(&(_, y)) => y == d.get_max(),
+        _             => false
+    }
 }
 
 fn setup_domain_simple() -> Domain {
-   Domain { min: -3, max: 64, intervals: [(-3, 2), (4, 42), (54, 64)] }
+    Domain {
+        dom: RefCell::new(Dom { min: -3, max: 64, intervals: vec![(-3, 2), (4, 42), (54, 64)] })
+    }
 }
 
 fn teardown(d: &Domain) {
-   assert!(min_is_min(d));
-   assert!(max_is_max(d));
+    assert!(min_is_min(d));
+    assert!(max_is_max(d));
 }
 
 #[test]
 fn sets_min_lower() {
-   let mut d = setup_domain_simple();
-   d.set_min(-4);
-   assert!(d.min == -3);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_min(-4);
+    assert!(d.get_min() == -3);
+    teardown(&d);
 }
 
 #[test]
 fn sets_min_middle() {
-   let mut d = setup_domain_simple();
-   let values = [-2, 8, 42, 54, 64];
-   let lengths = [3, 2, 2, 1, 1];
-   let mut v : int;
-   for i in range(0, values.len()) {
-      v = values[i];
-      d.set_min(v);
-      assert!(d.min == v);
-      assert!(d.intervals.len() == lengths[i])
-   }
-   teardown(&d);
+    let d = setup_domain_simple();
+    let values = [-2, 8, 42, 54, 64];
+    let lengths = [3, 2, 2, 1, 1];
+    let mut v : int;
+    for i in range(0, values.len()) {
+        v = values[i];
+        d.set_min(v);
+        assert!(d.get_min() == v);
+        assert!(d.dom.borrow().intervals.len() == lengths[i])
+    }
+    teardown(&d);
 }
 
 #[test]
 fn sets_min_in_hole() {
-   let mut d = setup_domain_simple();
-   d.set_min(43);
-   assert!(d.min == 54);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_min(43);
+    assert!(d.get_min() == 54);
+    teardown(&d);
 }
 
 #[test]
 // #[should_fail]
 fn sets_min_too_high() {
-   let mut d = setup_domain_simple();
-   d.set_min(65);
-   assert!(d.min == -3);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_min(65);
+    assert!(d.get_min() == -3);
+    teardown(&d);
 }
 
 #[test]
 fn sets_max_higher() {
-   let mut d = setup_domain_simple();
-   d.set_max(65);
-   assert!(d.max == 64);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_max(65);
+    assert!(d.get_max() == 64);
+    teardown(&d);
 }
 
 #[test]
 fn sets_max_middle() {
-   let mut d = setup_domain_simple();
-   let values = [63, 54, 42, 8, -3];
-   let lengths = [3, 3, 2, 2, 1];
-   let mut v : int;
-   for i in range(0, values.len()) {
-      v = values[i];
-      d.set_max(v);
-      assert!(d.max == v);
-      assert!(d.intervals.len() == lengths[i])
-   }
-   teardown(&d);
+    let d = setup_domain_simple();
+    let values = [63, 54, 42, 8, -3];
+    let lengths = [3, 3, 2, 2, 1];
+    let mut v : int;
+    for i in range(0, values.len()) {
+        v = values[i];
+        d.set_max(v);
+        assert!(d.get_max() == v);
+        assert!(d.dom.borrow().intervals.len() == lengths[i])
+    }
+    teardown(&d);
 }
 
 #[test]
 fn sets_max_in_hole() {
-   let mut d = setup_domain_simple();
-   d.set_max(43);
-   assert!(d.max == 42);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_max(43);
+    assert!(d.get_max() == 42);
+    teardown(&d);
 }
 
 #[test]
 // #[should_fail]
 fn sets_max_too_low() {
-   let mut d = setup_domain_simple();
-   d.set_max(-4);
-   assert!(d.max == 64);
-   teardown(&d);
+    let d = setup_domain_simple();
+    d.set_max(-4);
+    assert!(d.get_max() == 64);
+    teardown(&d);
 }
 
 fn setup_domain_holy() -> Domain {
-   Domain {
-      min: -3,
-      max: 64,
-      intervals: [(-3, 2), (4, 18), (20, 24), (30, 30),
-         (32, 34), (36, 38), (40, 42), (54, 64)] }
+    Domain {
+        dom: RefCell::new(Dom {
+                 min: -3,
+                 max: 64,
+                 intervals: vec![(-3, 2), (4, 18), (20, 24), (30, 30),
+                 (32, 34), (36, 38), (40, 42), (54, 64)]
+             })
+    }
 }
 
 #[test]
 fn remove_outside() {
-   let mut d = setup_domain_holy();
-   let e = setup_domain_holy();
-   d.remove(-8);
-   d.remove(3);
-   d.remove(19);
-   d.remove(31);
-   d.remove(35);
-   d.remove(48);
-   d.remove(128);
-   assert!(d.intervals.len() == e.intervals.len());
-   for i in range(0, d.intervals.len()) {
-      assert!(d.intervals[i] == e.intervals[i]);
-   }
-   teardown(&d);
+    let d = setup_domain_holy();
+    let e = setup_domain_holy();
+    d.remove(-8);
+    d.remove(3);
+    d.remove(19);
+    d.remove(31);
+    d.remove(35);
+    d.remove(48);
+    d.remove(128);
+    assert!(d.dom.borrow().intervals.len() == e.dom.borrow().intervals.len());
+    for i in range(0, d.dom.borrow().intervals.len()) {
+        assert!(d.dom.borrow().intervals.get(i) == e.dom.borrow().intervals.get(i));
+    }
+    teardown(&d);
 }
 
 #[test]
 fn remove_inside() {
-   let mut d = setup_domain_holy();
-   let values = [-3, -1, 30, 36, 64];
-   for &v in values.iter() {
-      d.remove(v)
-   }
-   for &v in values.iter() {
-      for &(x, y) in d.intervals.iter() {
-         assert!(v < x || v > y, format!("{} is not outside [{}..{}]", v, x, y));
-      }
-   }
-   assert!(d.intervals.len() == 8);
-   teardown(&d);
+    let d = setup_domain_holy();
+    let values = [-3, -1, 30, 36, 64];
+    for &v in values.iter() {
+        d.remove(v)
+    }
+    for &v in values.iter() {
+        for &(x, y) in d.dom.borrow().intervals.iter() {
+            assert!(v < x || v > y, format!("{} is not outside [{}..{}]", v, x, y));
+        }
+    }
+    assert!(d.dom.borrow().intervals.len() == 8);
+    teardown(&d);
 }
 
 #[test]
 fn it_does_propagate() {
-   let x = FDVar::new(-2, 255, "x");
-   let y = FDVar::new(-2, 255, "y");
-   let p = LtXYx::new(x, y);
-   assert!(x.max() == 254);
+    let x = FDVar::new(-2, 255, "x");
+    let y = FDVar::new(-2, 255, "y");
+    LtXYx::new(&x, &y);
+    assert!(x.max() == 254);
 }
